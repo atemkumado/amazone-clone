22ff1083d4fe9049d671c087be9a670c23c1678e
atemkumado <kumadnguyen123@gmail.com>
2020-12-26 13:15:03 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 /* eslint-disable jsx-a11y/alt-text */
 
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 function App() {
   return (
-    <div className='header'>
-      <img className='header__logo' src='http://pngimg.com//uploads/amazon/amazon_PNG11.png'/>
-      <div className= 'header__search'>
-        <input className="header__searchInput" type="Text" />
-      </div>
-
     
-    </div>
   );
 }
 
