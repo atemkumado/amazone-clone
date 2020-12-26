39eaec723d7cbbeb69d4e783393dc3dac2cd2f08
atemkumado <kumadnguyen123@gmail.com>
2020-12-26 12:48:44 +0700

    not thing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+/* eslint-disable jsx-a11y/alt-text */
+
 import React from 'react';
 import './Header.css';
 function App() {
   return (
-    <div className="header">
+    <div className='header'>
+      <img className='header__logo' src='http://pngimg.com//uploads/amazon/amazon_PNG11.png'/>
+      <div className= 'header__search'>
+        <input className="header__searchInput" type="Text" />
+      </div>
 
+    
     </div>
   );
 }
