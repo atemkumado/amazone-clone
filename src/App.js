4f4d0ac67ed2893ca7c18802d23f166a2f892d73
atemkumado <kumadnguyen123@gmail.com>
2020-12-26 15:43:09 +0700

    format header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 /* eslint-disable jsx-a11y/alt-text */
-
+import "./App.js";
 import React from "react";
-import "./Header.css";
+import Header from "./Header";
+
 function App() {
   return (
-    
+    //BEM
+    <div className="app">
+      <Header />
+    </div>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Header() {
   return (
@@ -10,7 +12,9 @@ function Header() {
       />
       <div className="header__search">
         <input className="header__searchInput" type="Text" />
+        <SearchIcon className="header__searchIcon" />
       </div>
+
       <div className="header__nav">
         <div className="header__option">
           <span className="header__optionLineOne">Hello Guest</span>
