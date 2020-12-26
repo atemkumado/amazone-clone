903c90e7a7e20e21228b8194b900f4f93eb772c2
atemkumado <kumadnguyen123@gmail.com>
2020-12-26 12:06:00 +0700

    push

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB8lvNyfL0fFR2GM9XChISAHDY9BGu_qio",
+    authDomain: "clone-8c895.firebaseapp.com",
+    projectId: "clone-8c895",
+    storageBucket: "clone-8c895.appspot.com",
+    messagingSenderId: "331759098732",
+    appId: "1:331759098732:web:76f4828d5795a37bafb149",
+    measurementId: "G-2YF9YPTXY7"
+  };
