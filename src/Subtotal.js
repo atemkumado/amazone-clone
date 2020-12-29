fcd246b01525e9f4b7cf0abfcd62aeb247559baa
atemkumado <kumadnguyen123@gmail.com>
2020-12-29 13:09:33 +0700

    subtotal

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -2,7 +2,9 @@ import React from "react";
 import "/Subtotal.css";
 
 function Subtotal() {
-  return <div></div>;
+  return <div className="subtotal">
+      
+  </div>;
 }
 
 export default Subtotal;
