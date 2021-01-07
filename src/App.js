4f7cd29bd96bf464dd72b1baab87959186e67de7
atemkumado <kumadnguyen123@gmail.com>
2021-01-08 00:48:51 +0700

    update login feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 /* eslint-disable jsx-a11y/alt-text */
 import "./App.js";
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
 import Home from "./Home";
 import Login from "./Login";
 import Checkout from "./Checkout";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { auth } from "./firebase.js";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    //will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("The USER: ", authUser);
+      if (authUser) {
+        // the user just/was logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     //BEM
     <Router>
       <div className="app">
+        <Header />
         <Switch>
           <Route path="/login">
             <Login />
           </Route>
-
-          <Header />
           <Route path="/checkout">
             <Checkout />
           </Route>
--- src/reducer.js
+++ src/reducer.js
@@ -37,6 +37,11 @@ const reducer = (state, action) => {
         ...state,
         basket: newBasket,
       };
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
     default:
       return state;
   }
