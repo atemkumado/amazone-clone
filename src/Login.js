fde97d3cdce96ef094389c96a13add62a504d67b
atemkumado <kumadnguyen123@gmail.com>
2021-01-07 13:38:16 +0700

    configure firebase

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = (e) => {
+    e.preventDefault();
+    //do some fancy firebase
+  };
+  const register = (e) => {
+    e.preventDefault();
+    //do some fancy firebase
+  };
   return (
     <div className="login">
       <Link to="/">
@@ -11,13 +21,39 @@ function Login() {
           alt="Amazone Logo"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1200px-Amazon_logo.svg.png"
         />
-        <div className="login__container">
-          <h1>Sign-in</h1>
-          <form>
-              
-          </form>
-        </div>
       </Link>
+      <div className="login__container">
+        <h1>Sign-in</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button
+            className="login__signInButton"
+            type="submit"
+            onClick={signIn}
+          >
+            Sign in
+          </button>
+        </form>
+        <p>
+          By creating an account, you agree to Amazon's Conditions of Use and
+          Privacy Notice.
+        </p>
+        <button onClick={register} className="login__registerButton">
+          Create your Amazon Account
+        </button>
+      </div>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,12 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
 const firebaseConfig = {
-    apiKey: "AIzaSyB8lvNyfL0fFR2GM9XChISAHDY9BGu_qio",
-    authDomain: "clone-8c895.firebaseapp.com",
-    projectId: "clone-8c895",
-    storageBucket: "clone-8c895.appspot.com",
-    messagingSenderId: "331759098732",
-    appId: "1:331759098732:web:76f4828d5795a37bafb149",
-    measurementId: "G-2YF9YPTXY7"
-  };
+  apiKey: "AIzaSyB8lvNyfL0fFR2GM9XChISAHDY9BGu_qio",
+  authDomain: "clone-8c895.firebaseapp.com",
+  projectId: "clone-8c895",
+  storageBucket: "clone-8c895.appspot.com",
+  messagingSenderId: "331759098732",
+  appId: "1:331759098732:web:76f4828d5795a37bafb149",
+  measurementId: "G-2YF9YPTXY7",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
